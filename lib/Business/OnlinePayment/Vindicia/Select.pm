85496e4ee4d272728b9b9be789894f717561ae41
Jason Terry <jterry@bluehost.com>
2016-10-27 09:47:25 -0600

    make test hashes begin with "aaaaaa" so that a human can more easily identify that it was mocked

--- lib/Business/OnlinePayment/Vindicia/Select.pm
+++ lib/Business/OnlinePayment/Vindicia/Select.pm
@@ -596,14 +596,14 @@ our $common_mock = {
               'return' => {
                             'returnString' => 'OK',
                             'returnCode' => '200',
-                            'soapId' => '75e2b24817abcba350f9bded7024a44d9e03b42b',
+                            'soapId' => 'aaaaaa4817abcba350f9bded7024a44d9e03b42b',
                           },
         },
         ok_duplicate => {
               'return' => {
                             'returnString' => 'OK',
                             'returnCode' => '200',
-                            'soapId' => '75e2b24817abcba350f9bded7024a44d9e03b42b',
+                            'soapId' => 'aaaaaa4817abcba350f9bded7024a44d9e03b42b',
                           },
               'response' => [
                               {
@@ -619,7 +619,7 @@ our $common_mock = {
               'return' => {
                             'returnString' => 'OK',
                             'returnCode' => '200',
-                            'soapId' => '75e2b24817abcba350f9bded7024a44d9e03b42b',
+                            'soapId' => 'aaaaaa4817abcba350f9bded7024a44d9e03b42b',
                           },
         },
     },
@@ -628,7 +628,7 @@ our $common_mock = {
               'return' => {
                             'returnString' => 'OK',
                             'returnCode' => '200',
-                            'soapId' => '75e2b24817abcba350f9bded7024a44d9e03b42b',
+                            'soapId' => 'aaaaaa4817abcba350f9bded7024a44d9e03b42b',
                           },
         },
     },
@@ -637,7 +637,7 @@ our $common_mock = {
           'return' => {
                         'returnCode' => '200',
                         'returnString' => 'OK',
-                        'soapId' => '141d55727eca030a16ddad54e4eaf8088a5fa322'
+                        'soapId' => 'aaaaaa727eca030a16ddad54e4eaf8088a5fa322'
                       },
           'transaction' => {
                            'currency' => 'USD',
@@ -646,7 +646,7 @@ our $common_mock = {
                            'amount' => '9000',
                            'subscriptionId' => 'TEST-1477513825.95775',
                            'paymentMethodIsTokenized' => '0',
-                           'creditCardAccountHash' => '68bfb396f35af3876fc509665b3dc23a0930aab1',
+                           'creditCardAccountHash' => 'aaaaaa96f35af3876fc509665b3dc23a0930aab1',
                            'nameValues' => {
                                            'vin:BillingCycle' => '0',
                                            'vin:RetryNumber' => '0'
@@ -656,7 +656,7 @@ our $common_mock = {
                            'subscriptionStartDate' => '2016-10-26T13:30:26-07:00',
                            'creditCardAccount' => '411111XXXXXX1111',
                            'status' => 'Failed',
-                           'VID' => '3ccefe5286ba2a8199a651d9f7afbee9a015fbb2',
+                           'VID' => 'aaaaaa5286ba2a8199a651d9f7afbee9a015fbb2',
                            'customerId' => '123',
                            'timestamp' => '2012-09-11T15:34:32-07:00',
                            'merchantTransactionId' => 'TEST-1477513825.95777'
